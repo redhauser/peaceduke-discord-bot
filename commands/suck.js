b006b89bb0fb0fc9cd9f6eff8974b491096e3156
redhauser <redhauser@gmail.com>
2022-03-08 15:41:54 +0200

    Updated suck.js in order to hide identities.

--- commands/suck.js
+++ commands/suck.js
@@ -5,10 +5,9 @@ module.exports = {
     .setName("suck")
     .setDescription("suck some dicks"),
     category: "розваги",
-    async execute(message, args) {
-        //hitler id - 640579047948288010 ; asstour4ik id - 552472613708890113
-        if(message.member.id === "640579047948288010") {
-            let asstour4ik = message.guild.members.cache.get("552472613708890113");
+    async execute(message, args, Discord, client, player, config) {
+        if(message.member.id === config.artemID) {
+            let asstour4ik = message.guild.members.cache.get(config.arturID);
             if(!asstour4ik || !asstour4ik.voice) return await message.reply("Він і так уже сакнув.");
             await asstour4ik.voice.disconnect();
             await message.reply({content: "артурчик вдало сакнув дік :thumbsup:", ephemeral: true});
