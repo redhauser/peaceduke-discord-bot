ef277775a7fa12ffe09f51f4d2a75d866f3fdfbd
redhauser <redhauser@gmail.com>
2022-03-09 23:49:16 +0200

    Minor bug fixes and changes.

--- commands/suck.js
+++ commands/suck.js
@@ -7,10 +7,10 @@ module.exports = {
     category: "розваги",
     async execute(message, args, Discord, client, player, config) {
         //Replace names with placeholders
-        if(message.member.id === config.artemID) {
-            let asstour4ik = message.guild.members.cache.get(config.arturID);
-            if(!asstour4ik || !asstour4ik.voice) return await message.reply("Він і так уже сакнув.");
-            await asstour4ik.voice.disconnect();
+        if(message.member.id === config.specialuser1ID) {
+            let user = message.guild.members.cache.get(config.specialuser2ID);
+            if(!user || !user.voice) return await message.reply("Він і так уже сакнув.");
+            await user.voice.disconnect();
             await message.reply({content: "артурчик вдало сакнув дік :thumbsup:", ephemeral: true});
         } else {
             await message.reply({content: "сак сом дікс"});
